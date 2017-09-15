66b9889b10fe7046c490f49226fcab77607e72b4
DingTobest <ding798513@163.com>
2017-09-15 10:00:22 +0800

    规范化命名规则

--- ats.main.py
+++ ats.main.py
@@ -9,42 +9,42 @@
 
 from rqalpha import main
 from rqalpha.utils.config import parse_config
-import batchAlgoTrade
+import batch_algo_trade
 
 import rqalpha
 
-def atsmain(strategyPath):
-    configInfo = parse_config({}, strategyPath)
-    main.run(configInfo)
+def atsmain(strategy_path):
+    config_info = parse_config({}, strategy_path)
+    main.run(config_info)
 
-def batchExec(strategyName):
-    StrategyNameArray = []
+def batch_exec(strategy_name):
+    strategy_name_array = []
 
-    configArray = batchAlgoTrade.setStrategyConfig(strategyName)
-    for index in range(len(configArray)):
-        strategyInfo = configArray[index]
-        print("准备执行的策略==>" + strategyInfo)
-        pos1 = strategyInfo.rfind('\\') + 1
-        pos2 = strategyInfo.rfind('.')
-        StrategyNameArray.append(strategyInfo[pos1: pos2])
+    config_array = batch_algo_trade.set_strategy_config(strategy_name)
+    for index in range(len(config_array)):
+        strategy_info = config_array[index]
+        print("准备执行的策略==>" + strategy_info)
+        pos1 = strategy_info.rfind('\\') + 1
+        pos2 = strategy_info.rfind('.')
+        strategy_name_array.append(strategy_info[pos1: pos2])
         # configInfo = parse_config({}, strategyInfo)
         # main.run(configInfo)
         print("执行结束")
 
     print("----------开始生成组合报告----------")
-    batchAlgoTrade.showAlgoTradeResult(StrategyNameArray)
+    batch_algo_trade.showAlgoTradeResult(strategy_name_array)
 
 if __name__ == '__main__':
     # 策略执行方式："singal"-执行单个策略，"batch"-批量执行策略，"download"-下载日线数据
-    execFlag = "singal"
+    exec_flag = "singal"
 
-    if execFlag == "singal":
+    if exec_flag == "singal":
         # 单个执行回测
         atsmain("D:\\OpenSourceTradePlatform\\atspy\\rqalpha\\config.yml")
-    elif execFlag == "batch":
+    elif exec_flag == "batch":
         # 批量执行回测
-        batchExec("ma20")
-    elif execFlag == "download":
+        batch_exec("ma20")
+    elif exec_flag == "download":
         # 下载日线数据
         main.update_bundle("D:\\OpenSourceTradePlatform\\atspy\\rqalpha")  # 获取回测数据
     else:
