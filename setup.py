5acd9761012dc7dc0663e24cca842f6efd3c371b
DingTobest <ding798513@163.com>
2018-05-25 10:23:37 +0800

    修改程序注册信息

---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Ricequant, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from os.path import dirname, join
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+
+with open(join(dirname(__file__), 'rqalpha/VERSION.txt'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
+
+requirements = [str(ir.req) for ir in parse_requirements("requirements.txt", session=False)]
+
+if sys.version_info.major == 2:
+    requirements += [str(ir.req) for ir in parse_requirements("requirements-py2.txt", session=False)]
+
+setup(
+    name='rqalpha',
+    version=version,
+    description='Ricequant Algorithm Trading System',
+    packages=find_packages(exclude=[]),
+    author='ricequant',
+    author_email='public@ricequant.com',
+    license='Apache License v2',
+    package_data={'': ['*.*']},
+    url='https://github.com/ricequant/rqalpha',
+    install_requires=requirements,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "rqalpha = rqalpha.__main__:entry_point"
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
